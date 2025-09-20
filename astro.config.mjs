0d19ba2596812dd4817875dfe19b448b07abc851
Gonzalo Fernández (CharlyGon) <Fernandez-gonza@hotmail.com>
2025-09-20 13:23:02 -0300

    feat: eliminar la integración con Netlify y optimizar la gestión del contador de visitas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
 import path from 'node:path';
 
 export default defineConfig({
-  output: 'hybrid',
-  adapter: netlify(),
   integrations: [tailwind()],
   vite: {
     resolve: {
